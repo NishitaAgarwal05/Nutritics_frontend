686e51af78814323cf22be691079a4ea34aae8a6
NishitaAgarwal05 <18ucs053@lnmiit.ac.in>
2022-06-22 20:53:16 +0530

    Register user updated

--- src/containers/userModule/RegisterUser.js
+++ src/containers/userModule/RegisterUser.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
-import axios from "axios";
+import {useDispatch} from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { registerUser } from "../../redux/services/user/userActions";
+import MyToast from "../MyToast";
 import {
   Row,
   Col,
@@ -9,7 +12,7 @@ import {
   Alert,
 } from "react-bootstrap";
 import {
-    FaSignInAlt,
+    FaUserPlus,
     FaEnvelope,
     FaLock,
     FaUndo,
@@ -17,7 +20,6 @@ import {
     FaPhone,
   } from "react-icons/fa"; 
 //import Joi from "joi-browser";
-
 const RegisterUser = (props) => {
   const [show, setShow] = useState(false);
   const [message, setMessage] = useState("");
@@ -30,178 +32,134 @@ const RegisterUser = (props) => {
   };
 
   const [user, setUser] = useState(initialState);
+
   const userChange = (event) => {
     const { name, value } = event.target;
     setUser({ ...user, [name]: value });
   };
 
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const saveUser = () => {
+    dispatch(registerUser(user))
+      .then((response) => {
+        setShow(true);
+        setMessage(response.message);
+        resetRegisterForm();
+        setTimeout(() => {
+          setShow(false);
+          navigate("/login");
+        }, 2000);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   const resetRegisterForm = () => {
     setUser(initialState);
   };
-
-  // state = {
-  //   user: {
-  //     userIdentification:"",
-  //     name:"",
-  //     email:"",
-  //     contact:"",
-  //     gender:"",
-  //     dob:"",
-  //     photo:"",
-  //     role:"user",
-  //     status:"active",
-  //     weight:"",
-  //     height:"",
-  //     intensity:"",
-  //     goal:"",
-  //     workOutTime:"",
-  //     wakeUpTime:"",
-  //     sleepTime:"",
-  //     medicalCondition:"",
-  //     allergicTo:"",
-  //     loginName:"",
-  //     password:"",
-  //     diateryOrientation:""
-  //   },
-  //   errors: {},
-  //   errMsg: "",
-  // };
-  
-  // // schema = {
-  // //   name: Joi.string().required(),
-  // //   email: Joi.string(3).required(),
-  // //   contact:Joi.integer().required(),
-
-
-  // // };
-
-  // //  // validate post with schema
-  // //  validate = () => {
-  // //   const errors = {};
-  // //   const result = Joi.validate(this.state.user, this.schema, {
-  // //     abortEarly: false,
-  // //   });
-  // //   console.log(result);
-  // //   if (result.error != null)
-  // //     for (let item of result.error.details) {
-  // //       errors[item.path[0]] = item.message;
-  // //     }
-  // //   return Object.keys(errors).length === 0 ? null : errors;
-  
-  // // };
-
-  // handleChange = (event) => {
-  //   // console.log("handleChange");
-  //   // console.log(event);
-  //   // console.log(event.target.name); //return name of the field
-  //   // console.log(event.target.value); //return value entered by the user
-  //   const newUser = { ...this.state.user };
-  //   newUser[event.target.name] = event.target.value;
-  //   this.setState({ user: newUser });
-  // };
-
-  // handleSubmit = (event) => {
-  //   event.preventDefault();
-  //   console.log("handleSubmit");
-  //  // update state with errors after validation
-  //   this.setState({ errors: this.validate() });
-
     return (
-      <Row className="justify-content-md-center" style={{margin:"10vh", height:"100vh"}}>
-      <Col xs={5}>
-      <Card className={"border border-dark bg-dark text-white"}>
-      <Card.Header>
-        <div style={{float:"left"}}>
-          <FaUser ></FaUser> Register
+      <div>
+        <div style={{ display: show ? "block" : "none" }}>
+          <MyToast show={show} message={message} type={"success"} />
         </div>
-        </Card.Header>
-      <Card.Body>
-        <Form>
-        <Form.Group className="mb-3  ">
-            <div className="input-group mb-3">
-              <div className="input-group-prepend">
-                <span className="input-group-text" ><i className="bi bi-aspect-ratio"><FaUser ></FaUser></i></span>
-              </div>
-              <input 
-                type="text"
-                name="name"
-                className="form-control" 
-                value={user.name}
-                onChange={userChange}
-                placeholder="Name"
-                />
-            </div>
-          </Form.Group>
+        <Row className="justify-content-md-center" style={{margin:"10vh", height:"100vh"}}>
+        <Col xs={5}>
+        <Card className={"border border-dark bg-dark text-white"}>
+        <Card.Header>
+          <div style={{float:"left"}}>
+            <FaUser ></FaUser> Register
+          </div>
+          </Card.Header>
+        <Card.Body>
+          <Form>
           <Form.Group className="mb-3  ">
-            <div className="input-group mb-3">
-              <div className="input-group-prepend">
-                <span className="input-group-text" ><i className="bi bi-aspect-ratio"><FaEnvelope ></FaEnvelope></i></span>
+              <div className="input-group mb-3">
+                <div className="input-group-prepend">
+                  <span className="input-group-text" ><i className="bi bi-aspect-ratio"><FaUser ></FaUser></i></span>
+                </div>
+                <input 
+                  type="text"
+                  name="name"
+                  className="form-control" 
+                  value={user.name}
+                  onChange={userChange}
+                  placeholder="Name"
+                  />
               </div>
-              <input 
-                type="email" 
-                name="email"
-                className="form-control" 
-                placeholder="Email" 
-                value={user.email}
-                onChange={userChange}
-                />
-            </div>
-          </Form.Group>
-          <Form.Group className="mb-3  ">
-            <div className="input-group mb-3">
-              <div className="input-group-prepend">
-                <span className="input-group-text" ><i className="bi bi-aspect-ratio"><FaLock></FaLock></i></span>
+            </Form.Group>
+            <Form.Group className="mb-3  ">
+              <div className="input-group mb-3">
+                <div className="input-group-prepend">
+                  <span className="input-group-text" ><i className="bi bi-aspect-ratio"><FaEnvelope ></FaEnvelope></i></span>
+                </div>
+                <input 
+                  type="email" 
+                  name="email"
+                  className="form-control" 
+                  placeholder="Email" 
+                  value={user.email}
+                  onChange={userChange}
+                  />
               </div>
-              <input 
-                type="password" 
-                name="password"
-                className="form-control" 
-                placeholder="Password" 
-                value={user.password}
-                onChange={userChange}
-                />
-            </div>
-          </Form.Group>
-          <Form.Group className="mb-3  ">
-            <div className="input-group mb-3">
-              <div className="input-group-prepend">
-                <span className="input-group-text" ><i className="bi bi-aspect-ratio"><FaPhone></FaPhone></i></span>
+            </Form.Group>
+            <Form.Group className="mb-3  ">
+              <div className="input-group mb-3">
+                <div className="input-group-prepend">
+                  <span className="input-group-text" ><i className="bi bi-aspect-ratio"><FaLock></FaLock></i></span>
+                </div>
+                <input 
+                  type="password" 
+                  name="password"
+                  className="form-control" 
+                  placeholder="Password" 
+                  value={user.password}
+                  onChange={userChange}
+                  />
               </div>
-              <input 
-                type="text"
-                name="mobile"
-                className="form-control" 
-                placeholder="Mobile Number"
-                value={user.mobile}
-                onChange={userChange}
-                />
-            </div>
-          </Form.Group>
-        </Form>
-        </Card.Body>
-        <Card.Footer style={{ textAlign: "right" }}>
-           <Button
-            size="sm"
-            type="button"
-            variant="success"
-            // onClick={validateUser}
-            disabled={user.email.length === 0 || user.password.length === 0}
-          >
-            <FaSignInAlt></FaSignInAlt> Login
-          </Button>{" "}
-          <Button
-            size="sm"
-            type="button"
-            variant="primary"
-            onClick={resetRegisterForm}
-            disabled={user.email.length === 0 && user.password.length === 0}
-          >
-            <FaUndo></FaUndo> Reset
-          </Button>
-        </Card.Footer>
-        </Card>
-      </Col>
-    </Row>
+            </Form.Group>
+            <Form.Group className="mb-3  ">
+              <div className="input-group mb-3">
+                <div className="input-group-prepend">
+                  <span className="input-group-text" ><i className="bi bi-aspect-ratio"><FaPhone></FaPhone></i></span>
+                </div>
+                <input 
+                  type="text"
+                  name="mobile"
+                  className="form-control" 
+                  placeholder="Mobile Number"
+                  value={user.mobile}
+                  onChange={userChange}
+                  />
+              </div>
+            </Form.Group>
+          </Form>
+          </Card.Body>
+          <Card.Footer style={{ textAlign: "right" }}>
+            <Button
+              size="sm"
+              type="button"
+              variant="success"
+              onClick={saveUser}
+              disabled={user.email.length === 0 || user.password.length === 0}
+            >
+              <FaUserPlus></FaUserPlus> Register
+            </Button>{" "}
+            <Button
+              size="sm"
+              type="button"
+              variant="primary"
+              onClick={resetRegisterForm}
+            >
+              <FaUndo></FaUndo> Reset
+            </Button>
+          </Card.Footer>
+          </Card>
+        </Col>
+      </Row>
+    </div>
     );
   }
 
